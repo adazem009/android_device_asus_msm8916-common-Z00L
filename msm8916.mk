65a7950e055acf4ffd1448b441e89ae8b9e4e33b
markkiths <markkiths@gmail.com>
2020-11-27 13:36:38 +0700

    msm8916-common: Update overlay for AEX/PE

--- msm8916.mk
+++ msm8916.mk
@@ -29,8 +29,7 @@ endif
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \

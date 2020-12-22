32d1ef9672e18dcf0125b2d4c1b4933e46f56034
Roman Kiryanov <rkir@google.com>
2020-12-22 16:52:07 +0000

    msm8916-common: Replace audio@2.0-service with audio.service
    Legacy service name, use android.hardware.audio.service instead
    
    Bug: 153579444
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Merged-In: I4c05accb86dd0c9cd1b3fef3d84e0d54b2afc6a1
    Change-Id: I6b55b4b1db2577f4176c99dca7021cfe78e13307

--- msm8916.mk
+++ msm8916.mk
@@ -86,7 +86,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
-    android.hardware.audio@2.0-service \
+    android.hardware.audio@.service \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl \

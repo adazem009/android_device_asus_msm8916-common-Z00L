LineageOS/android_device_motorola_msm8916-common

msm8916-common: Add boringssl-compat lib for the JPEG encoder
Added shim to device tree

This tweaks and optimize camera quality on our moto 8916 devices
edit: adapt to the new shim method

Co-Authored By: althafvly <althafvly@gmail.com>
Change-Id: I06f4317c7f65d0225b04ec2ddee4479422fa4b7f

BoardConfigCommon.mk
@@ -150,7 +150,13 @@ TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/libmot_sensorlistener.so|libsensor.so \
     /system/vendor/lib/libjustshoot.so|libshims_camera.so \
     /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so \
-    /system/vendor/lib/libizat_core.so|libshims_get_process_name.so
+    /system/vendor/lib/libizat_core.so|libshims_get_process_name.so \
+    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
+    /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so
+
+# Sp-shim
+TARGET_LD_PRELOAD := \
+    /system/lib/libboringssl-compat.so
 
 # Vendor Interface Manifest
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml

adazem009/android_device_asus_msm8916-common-Z00L

BoardConfigCommon.mk
@@ -198,7 +198,13 @@ endif
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib64/lib-imscamera.so|libshims_camera.so \
     /system/vendor/lib64/libflp.so|libshims_flp.so \
-    /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so
+    /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so \
+    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
+    /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so
+
+# Sp-shim
+TARGET_LD_PRELOAD := \
+    /system/lib/libboringssl-compat.so
 
 # Vendor Unification Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8916
markkiths/android_device_asus_msm8916-common

msm8916-common: props: Add persist.hwc.ptor.enable and debug.enable.sglscale
I didn't find who added it to cyanogen msm8916-common. D:
https://github.com/LineageOS/android_device_cyanogen_msm8916-common/blob/e92cee4eaf6f6d58e3bbbf9d0e81fce013a007f3/system_prop.mk#L26

system_prop.mk
@@ -57,6 +57,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.enable.sglscale=1 \
     debug.hwui.use_buffer_age=false \
     debug.egl.hw=0 \
     debug.enable.sglscale=1 \
@@ -72,6 +73,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
     persist.hwc.mdpcomp.enable=true \
+    persist.hwc.ptor.enable=true \
     ro.opengles.version=196608
 
 # DRM

adazem009/android_device_asus_msm8916-common-Z00L

system_prop.mk
@@ -72,7 +72,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
-    persist.hwc.mdpcomp.enable=true
+    persist.hwc.mdpcomp.enable=true \
+    persist.hwc.ptor.enable=true
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \